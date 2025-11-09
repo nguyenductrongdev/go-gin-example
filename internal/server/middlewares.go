78075767718cac261e85c25be11abf90ea91220e
ndtrong <nguyenductrong12a1@gmail.com>
2025-11-10 00:47:06 +0700

    feat: support both rest token and ws token

--- internal/server/middlewares.go
+++ internal/server/middlewares.go
@@ -14,11 +14,18 @@ func Authenticated() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		currentUserId := "00000000-0000-0000-0000-000000000000"
 
-		gatewayUserId := c.GetHeader("X-Ws-User-Id")
-		if gatewayUserId != "" {
-			log.Printf("Gateway authenticated user %v", gatewayUserId)
-			currentUserId = gatewayUserId
+		gatewayWsUserId := c.GetHeader("X-Ws-User-Id")
+		gatewayRestUserId := c.GetHeader("X-User-Id")
 
+		log.Printf("--- X-Ws-User-Id: %v", gatewayWsUserId)
+		log.Printf("--- X-User-Id: %v", gatewayRestUserId)
+
+		if gatewayWsUserId != "" {
+			log.Printf("Gateway ws authenticated user %v", gatewayWsUserId)
+			currentUserId = gatewayWsUserId
+		} else if gatewayRestUserId != "" {
+			log.Printf("Gateway rest authenticated user %v", gatewayRestUserId)
+			currentUserId = gatewayRestUserId
 		} else {
 			token := c.GetHeader("Authorization")
 
